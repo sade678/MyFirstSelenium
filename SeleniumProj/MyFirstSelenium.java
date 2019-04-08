148bb042f7043d3695e505ff7117d017c8585fab
sade678 <sadequr1133@gmail.com>
2019-04-08 11:17:23 -0400

    Selenium Proj

---
+++ SeleniumProj/MyFirstSelenium.java
@@ -0,0 +1,15 @@
+package SeleniumProj;
+
+public class MyFirstSelenium {
+
+	public static void main(String[] args) {
+		
+		String name = "hello selenium";
+		System.out.println("hello selenium");
+
+		
+		
+		
+	}
+
+}
